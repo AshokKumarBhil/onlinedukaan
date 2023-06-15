86c4e2266a78085bb6856996e8737824fd2e54e3
Ashok <ashokkumarbhil50@gmail.com>
2023-06-15 21:06:29 +0530

    commit for new

--- src/main/java/com/onlinedukaan/controllers/UserController.java
+++ src/main/java/com/onlinedukaan/controllers/UserController.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.security.Principal;
